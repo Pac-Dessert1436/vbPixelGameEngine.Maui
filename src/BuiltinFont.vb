473c72a77fd6a6ad40586f38e4e3b66a2d3d9f86
Pac-Dessert1436 <benjamin_2001@qq.com>
2026-02-09 07:19:06 +0800

    Refactor Stella.vb & add 1 more pixel font

--- src/BuiltinFont.vb
+++ src/BuiltinFont.vb
@@ -71,7 +71,7 @@ Friend Module BuiltinFontExtensions
           .Append("O`000P04Od400g`63V=P0GP373IP0`@3>1`00P@6O`P00g`4O`000GP000000000")
           .Append("?P90L020O`<PN3R0@E4@C4B0@ET8ATB0@@T4C4B0O`l2N7b0?PH1L3R000000020")
         Case BuiltinFont.Contra
-          ' Done: Add font sheet from Contra / Time Pilot '84
+          ' Done: Font sheet from Contra / Time Pilot '84
           .Append("73`0`01P8Wh0i01iBD<0?0LOEDT090\CCF40900ADCl0?00K7Qh0600>00000000")
           .Append("L000430h6010>7Qn5F8L;4a34g8>96AAOgh4i3@aO`<0n7Qo@040W4a>00000000")
           .Append("@600061PNG8;I3UiCgT7O5l?BDT024\IBDd;14AiOdl733a_=PH061P600000000")
@@ -89,7 +89,7 @@ Friend Module BuiltinFontExtensions
           .Append("L00040@0300027`47b9P37P6O3IP70`1L1`060@670P027P40`00140000000000")
           .Append(">010H300OP1PF7R0@d8@A4b0@A48@TB0H@T4A4B0?`l3F3b03PH1H1R000000000")
         Case BuiltinFont.SonicPocket
-          ' Done: Add font sheet from Sonic Pocket Adventure
+          ' Done: Font sheet from Sonic Pocket Adventure
           .Append("?Sh0O01o@Gl0O01oGDT0O0=oEDD050LACGl070PODGl0700O7Sh0200>00000000")
           .Append("H000220n744077Ao4gl075Ao7gm_55A9Ogm_O7aoO41_O7`oH000O7Qn00000000")
           .Append("OgT0O7moOgd7O7moOgd3O7moADD024@9OdL737aoOdL337ao>T8033Qf00000000")
@@ -107,7 +107,7 @@ Friend Module BuiltinFontExtensions
           .Append("O`0027`83d4017`47b9P17`2?QAP20@1O0P047`2N00047`4O`0027P800000000")
           .Append("?P9P<3R0O`<`>7b0OeTH?7b0@ET<?TB0Oel6?7b0O`l3>7b0?PH1<3R000000020")
         Case BuiltinFont.Xevious
-          ' Done: Add font sheet from Xevious (by Namco)
+          ' Done: Font sheet from Xevious (by Namco)
           .Append("03h0001o?7l0o01o@Wl0o0=oFT40A0LAFT40A0@AG440?00A03h0000>00000000")
           .Append("OP0>000n4@5OO7Ao4GmOO5Ao4Gl?A5A1Ogl6a7a1O`00o7aaOP00000n00000000")
           .Append("Og40001oOgT7O7l?OgT7O4L?BDT024@9BDl717aiBDl717ai=TH0001f00000000")
@@ -125,7 +125,7 @@ Friend Module BuiltinFontExtensions
           .Append("O`000P000@000G`40B9`0@@20AA`0P@1O`P00W`2O`000W`4OP000@0000000000")
           .Append("?P90L020O`4PB7b0Oe4@A4B0@ET8@TB0@@l4A7b0@@l2B7b0?PH1L02000000020")
         Case BuiltinFont.Gradius3
-          ' Done: Add font sheet from Gradius 3 (by Konami)
+          ' Done: Font sheet from Gradius 3 (by Konami)
           .Append("?Sh0001m@F40o009GE4090L9EDT090\9CDD09009DD<090097Sh0600600000000")
           .Append("H000000n<00063Q1:09@94A197l<94A18P0?94AA8@0392@QO`00o7aN00000400")
           .Append("OG44001mBDT3O7l9BDT324@9BDT014@9BDT414@9BDT314@I=TH313QV00000000")
@@ -143,8 +143,23 @@ Friend Module BuiltinFontExtensions
           .Append("O`0020080T4017`4129P10P221AP30@140P020@2800020@4O`0017P800000000")
           .Append("?P50L020@@4PB3R0@E4@A4B0@@T8@TB0@@T4A4B0@@T2B4B0?PH1L3R000000020")
         Case BuiltinFont.DigDug1996
-          ' ToDo: Add font sheet from Dig-Dug Arrangement 1996
-          .Append("")
+          ' Done: Font sheet from Dig-Dug Arrangement 1996
+          .Append("7000001o8Sh0o01oBGl0o0<9ED4090L9CD4090P9DD4090097Sh0600600000000")
+          .Append("L000000L7P0063Pn7d80?7aS4GmO94A14Gl794AA7T00o2@RL000P7aL00000000")
+          .Append("O`08005oOf8717moBG43N4P9BGT014@9BDl814@IBDl737`Y=TH323Q600000000")
+          .Append("700P03PV?R9d27a?@DT^G4A?@DTTE4A9@DUdE4Ai@Gl^E2Qi8SH4800b00000000")
+          .Append("O`0413P1OaP^17`1@AHZ?dAo@E5oA4Ao@GlZI2@18WlZ87l1750@000000000000")
+          .Append("O`1203POObLU?7`oBDLBO5APBDD8@5A0BGdT@5A0BGeB81PP@CTQO00O00000000")
+          .Append("O`0P00P7Ocif77hO2Dm9?7mh2DU9@0UP2GUF@0TH2GTP80870C9@700000000000")
+          .Append("7000001o?PL074PH@@48?9@6@G47@9AoDGl3>9@`L@L0@?P<4P<087`300007000")
+          .Append("O`000013OcHLA7lV24lVC10L24m3>0PH27U1<0Pd27T0J7QROcH0A71100000000")
+          .Append("0000000302I1C00<@DUQd0A`OdTbT7e`OdTLW60<@Gl0O00303h0000000000000")
+          .Append("<008A011@@0ZI41Q@FHLM<1aOfH8E819?`0LC8170@0ZA4E30@0803e100000000")
+          .Append("O`000000Oh0827mo27<8=Wmo23<n@A115008@C118P0804P0@@0004@000000000")
+          .Append("O`P00001Oa@00002@2:`0044@45`Mcl8@000060@@000060P@000001000000000")
+          .Append("O`000000Oa@8@Ga11Q@8@GQ161@8=PAo61@827ao1Q@800@0Oa@800@0000007P0")
+          .Append("O`002008Od4017`40R9P17P211AP20P120P040@2400040@4O`0027P800000000")
+          .Append("70I0L020?P4PB3R0He4@A7b0@@l8@TB0@@H4A4B08P02B4B07001L3R000000020")
         Case BuiltinFont.Flicky
           ' ToDo: Add font sheet from Flicky (by SEGA)
           .Append("")
